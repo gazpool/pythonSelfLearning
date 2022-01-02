1540716cdc86930b40ec39193f7c1082d8fbd590
gazpool <g.e.pool1@outlook.com>
2022-01-02 12:27:32 +0000

    Initial commit

---
+++ full_name.py
@@ -0,0 +1,8 @@
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f'{first_name} {last_name}'
+message =f'Hello, {full_name.title()}!'
+print(message)
+
+
+print("languages:\n\tpython\n\tC\n\tSQL")
---
+++ languages.py
@@ -0,0 +1,4 @@
+print("Languages:\n\tpython\n\tC\n\tSQL")
+
+favourite_language = 'python '
+favourite_language
