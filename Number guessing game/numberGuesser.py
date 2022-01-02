477d6a53b00ef10905ac85e4acf800ed4c71909b
gazpool <g.e.pool1@outlook.com>
2022-01-02 13:51:32 +0000

    Update numberGuesser.py

--- Number guessing game/numberGuesser.py
+++ Number guessing game/numberGuesser.py
@@ -1,7 +1,36 @@
 import random
+guesses = 0
 
-r = random.randrange(1,10) #random num 1-9, 
+print("Welcome to the number guessing game!")
+topOfRange = input("Pick a number: ")
+if topOfRange.isdigit():
+        topOfRange = int(topOfRange)
 
-print(r)
+        if topOfRange <= 0:
+            print('Oops, please type a number above 0')
+            quit()
+else: 
+    print('Oops, make sure you type a number next time')
 
-sdfsdfsdfsdf
+r = random.randint(0,topOfRange)
+
+while True:
+    guesses += 1
+    userGuess = input(f"Guess a number between 0 and {topOfRange} ")
+    if userGuess.isdigit():
+        userGuess = int(userGuess)
+    else: 
+        print('Oops, make sure you type a number next time')
+        continue
+    
+    if userGuess == r: 
+        print("Well done, you got it!")
+        break
+    else:
+        if userGuess < r:
+            print("Too low")
+        if userGuess > r:
+            print("Too high")
+
+print("You got it in", guesses, "guesses")
+
