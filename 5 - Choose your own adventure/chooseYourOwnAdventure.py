fb1dc10da135df5a421c25c17051450e43baa8ff
gazpool <g.e.pool1@outlook.com>
2022-01-06 17:54:20 +0000

    Update chooseYourOwnAdventure.py

--- 5 - Choose your own adventure/chooseYourOwnAdventure.py
+++ 5 - Choose your own adventure/chooseYourOwnAdventure.py
@@ -1,5 +1,20 @@
-#Variables
+userName = input("Welcome adventurer, what is your name? ")
+print(userName)
+answer = input(f"Well, {userName}, are you ready to embark on your journey? (yes/no) ")
 
-#Begin
-userName = input("Weclome adventurer, what is your name? ")
-input(f"Well, {userName}, are you ready to embark on your journey? (yes/no) ")
+if answer.lower == "no":
+    print("OK, maybe next time")
+elif answer.lower == "yes":
+    answer = input("You are walking along a dirt path and come to a fork in the road, which way do you go? (left/right)? ")
+
+if answer.lower == "left":
+    answer = input("You come accross a fast flowing river, do you want to try swim accross or walk around until you find a crossing (swim/ walk)? ")
+    if answer.lower == "swim":
+        print("You try to swim across but the current is too strong, you are sucked under the water and drown")
+        print("GAME OVER")
+    elif answer.lower == "walk":
+        print("You walk for miles without finiding a crossing, you realise you forgot to bring any water and do not have the energy to make it back")
+        print("GAME OVER")
+    else:
+        print("You failed to choose a valid option")
+        print("GAME OVER")
